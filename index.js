1d69159c768d543972843f3661ce35cc178d46b5
bobthesnek63 <56647593+bobthesnek63@users.noreply.github.com>
2020-07-10 20:09:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $("#enter").animate({
+    opacity: "0.9"
+  }, 3000);
+
+  $("#open").delay(1000).animate({
+    opacity: "0.9"
+  }, 3000);
+
+  $("#img").delay(7000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img2").delay(12000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img3").delay(17000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img4").delay(22000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img5").delay(27000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img6").delay(32000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img7").delay(37000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img8").delay(42000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#img9").delay(47000).animate({
+    opacity: "1"
+  }, 1000);
+
+  $("#end").delay(48000).animate({
+    opacity: "0.9"
+  }, 1000);
+
+});
